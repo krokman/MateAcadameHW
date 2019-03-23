3b110ad7b4b80457b668f4e1d3549d4c629c77a3
Hyspan <32776601+krokman@users.noreply.github.com>
2019-03-23 16:42:29 +0200

    Obj1

---
+++ src/HW2/ImmutableExample.java
@@ -0,0 +1,32 @@
+package HW2;
+
+/*
+2.4 Создать immutable класс
+ */
+
+// make this class final, no one can extend this class
+public final class ImmutableExample {
+
+	private String name;
+
+	ImmutableExample (String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	//no setter
+
+	public static void main(String[] args) {
+
+		ImmutableExample obj = new ImmutableExample("Immutable");
+		System.out.println(obj.getName());
+
+		// there is no way to update the name after the object is created.
+		// obj.setName("new mkyong");
+		// System.out.println(obj.getName());
+
+	}
+}
---
+++ src/HW2/RPGCharacter.java
@@ -0,0 +1,33 @@
+package HW2;
+
+public class RPGCharacter {
+
+	private String race = null;
+	private String characterClass = null;
+	private boolean sex = false;
+
+	public void setSex(boolean sex){
+		this.sex = sex;
+	}
+
+	public void setRace(String race){
+		this.race = race;
+	}
+
+	public void setCharacterClass(String characterClass){
+		this.characterClass = characterClass;
+	}
+
+	public String getRace() {
+		return race;
+	}
+
+	public String getCharacterClass() {
+		return characterClass;
+	}
+
+	public boolean isSex() {
+		return sex;
+	}
+}
+
