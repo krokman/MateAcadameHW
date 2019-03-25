77abc374572e2296316642f7ed9a94a4f921f2f0
Hyspan <32776601+krokman@users.noreply.github.com>
2019-03-25 12:47:01 +0200

    HW1 rework

--- src/HW1/Main.java
+++ src/HW1/Main.java
@@ -1,20 +1,19 @@
 package HW1;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main{
-/*
-*
- */
 	public static final Main start = new Main();
     public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException, CloneNotSupportedException{
-	    start.footballMatch(); //exс 1.1
-        start.rhombusDisplay(); //exc 1.2
-        start.degreeOf(); //exc 1.3
-        start.recursionDisplay(); //exc 1.4
-        start.matrixMinMaxOut(); //exc 1.6
+	    start.footballMatch(); // exс 1.1
+        start.rhombusDisplay(); // exc 1.2
+        start.degreeOf(); // exc 1.3
+        start.recursionDisplay(); // exc 1.4
+        start.matrixMinMaxOut(); // exc 1.6
 
         ObjectDeepClone object = new ObjectDeepClone("Tall ", 14, new DeepClone());
         ObjectDeepClone objectClone = object.clone();
@@ -26,38 +25,29 @@ public class Main{
         // exc 1.1
         System.out.println("Write match score X Y, and your score X Y");
 
-            int sc1 = Integer.parseInt(reader.readLine());
-            int sc2 = Integer.parseInt(reader.readLine());
-            int us1 = Integer.parseInt(reader.readLine());
-            int us2 = Integer.parseInt(reader.readLine());
+            int matchScore1 = Integer.parseInt(reader.readLine());
+            int matchScore2 = Integer.parseInt(reader.readLine());
+            int yourScore1 = Integer.parseInt(reader.readLine());
+            int yourScore2 = Integer.parseInt(reader.readLine());
 
-            int x = (sc1 == us1 && sc2 == us2 ? 2 : sc1 > sc2 && us1 > us2 ? 1 : sc1 < sc2 && us1 < us2 ? 1 : 0);
-            System.out.println(x);
+            int outScore = ((matchScore1 == yourScore1 && matchScore2 == yourScore2) ? 2 :
+                        (matchScore1 > matchScore2 && yourScore1 > yourScore2) ? 1 :
+                                (matchScore1 < matchScore2 && yourScore1 < yourScore2) ? 1 : 0);
+            System.out.println(outScore);
     }
 
     public void rhombusDisplay() throws IOException{
         // exc 1.2
         System.out.println("Write size of Rhombus");
 
-        int N = Integer.parseInt(reader.readLine());
-        float center = N / 2;
+        int border = Integer.parseInt(reader.readLine());
+        float center = border / 2;
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < border; i++) {
             System.out.println();
-            for (int j = 0; j < N; j++) {
-                if (i <= center) {
-                        // Top half rhombus
-                    if (j >= center - i && j <= center + i)
-                            System.out.print("*");
-                    else
-                            System.out.print(" ");
-                } else {
-                        // Lower half rhombus
-                    if (j >= center + i - N + 1 && j <= center - i + N - 1)
-                        System.out.print("*");
-                    else
-                        System.out.print(" ");
-                }
+            for (int j = 0; j < border; j++) {
+                System.out.print((i <= center)?(j >= center - i && j <= center + i)?"*":" ":
+                        (j >= center + i - border + 1 && j <= center - i + border - 1)?"*":" ");
             }
         }
     }
@@ -66,70 +56,76 @@ public class Main{
         // exc 1.3
         System.out.println("\nWrite number and degree");
 
-        int n = Integer.parseInt(reader.readLine());
+        int number = Integer.parseInt(reader.readLine());
         int degree = Integer.parseInt(reader.readLine());
-        int buff = n;
+        int buff = number;
 
-        if (degree == 0){n = 1;}
-        for (int i = 0; i < degree - 1; i++){
-            n *= buff;
+        if (degree == 0){
+            number = 1;
         }
-        System.out.println(n);
+        for (int i = 0; i < degree - 1; i++){
+            number *= buff;
+        }
+        System.out.println(number);
     }
 
-
-
-    public int factorial(int n){
+    public int factorial(int number){
         int result;
-
-        if (n == 1) return 1;
-
-        result = factorial(n-1) * n;
+        if (number == 1) {
+            return 1;
+        }
+        result = factorial(number-1) * number;
         return result;
     }
 
-    public long fibonacci(int n) {
-        if (n <= 1) return n;
-        else return fibonacci(n - 1) + fibonacci(n - 2);
+    public long fibonacci(int number){
+        if (number <= 1) {
+            return number;
+        }
+        else {
+            return fibonacci(number - 1) + fibonacci(number - 2);
+        }
     }
 
-    public int taylor(int n) {
-        if (n == 0) return 1;
-        return n * taylor(n-1);
+    public int taylor(int number){
+        if (number == 0) {
+            return 1;
+        }
+        return number * taylor(number-1);
     }
 
     public void recursionDisplay() throws IOException{
         System.out.println("Number for recursions");
-        int n1 = Integer.parseInt(reader.readLine());
+        int number = Integer.parseInt(reader.readLine());
 
         System.out.println("Fibonacci");
-        for (int i = 0; i <= n1; i ++){
+        for (int i = 0; i <= number; i++) {
             System.out.print(start.fibonacci(i) + " ");
         }
 
         System.out.println("\nTaylor sentence");
-        for (int i = 0; i <= n1; i ++){
+        for (int i = 0; i <= number; i++) {
             System.out.print(start.taylor(i) + " ");
         }
 
         System.out.println("\nFactorial");
-        System.out.println(start.factorial(n1));
+        System.out.println(start.factorial(number));
     }
 
     public void matrixMinMaxOut(){
        // exc 1.6
-        int n = 4;
-        int[][] matrix = new int[n][n];
+        int border = 4;
+        int[][] matrix = new int[border][border];
 
-        for (int i = 0; i < n; i++){ //initializing matrix with random numbers < 50
-            for (int j = 0; j < n; j++){
-                matrix[i][j] = (int)(Math.random()*50);
+        for (int i = 0; i < border; i++) { //initializing matrix with random numbers < 50
+            for (int j = 0; j < border; j++) {
+                matrix[i][j] = (int)(Math.random() * 50);
             }
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < border; i++) {
             System.out.println();
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < border; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
         }
@@ -137,13 +133,16 @@ public class Main{
         int min = matrix[0][0];
         int max = matrix[0][0];
 
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                if(matrix[i][j] > max){max = matrix[i][j];}
-                if(matrix[i][j] < min){min = matrix[i][j];}
+        for (int i = 0; i < border; i++) {
+            for (int j = 0; j < border; j++) {
+                if(matrix[i][j] > max){
+                    max = matrix[i][j];
+                }
+                if(matrix[i][j] < min){
+                    min = matrix[i][j];
+                }
             }
         }
         System.out.println("\nMax = " + max + " Min = " + min);
     }
-
 }
